0c11389b23bc13d03375e8369dfb4a716480b755
Mahdi516630 <mahdiyacoubali2004@gmail.com>
2025-06-24 16:41:37 +0300

    k

---
+++ CSV/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Charger les données CSV
+df = pd.read_csv('cv_unifie.csv')
+
+# Route principale : retourner toutes les données
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(df.to_dict(orient='records'))
+
+# Route par ID
+@app.route('/api/data/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = df[df['id'] == item_id]
+    if not item.empty:
+        return jsonify(item.to_dict(orient='records')[0])
+    return jsonify({'error': 'Item non trouvé'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- README.md
+++
@@ -1,3 +0,0 @@
-Bonsoir Groupe 4. Vous aviez fait un très bon travail. ainsi vous aviez bien suivie la structure proposé pour cette 1er partie à savoir.
-Mais y'a un petit soucis. Votre projet est similaire au projet de groupe 15. Pouvez-vous choisir un autre sujet ...? veuillez me contacter
-
